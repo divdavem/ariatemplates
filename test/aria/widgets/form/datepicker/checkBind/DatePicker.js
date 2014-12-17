6fc982821b1a520e3cb9e1e1861a316db3fb89f3
fbasso <fabrice.basso@amadeus.com>
2014-12-17 15:23:14 +0100

    test.aria.widgets.form.datepicker.checkBind.DatePicker

--- test/aria/widgets/form/datepicker/checkBind/DatePicker.js
+++ test/aria/widgets/form/datepicker/checkBind/DatePicker.js
@@ -31,7 +31,7 @@ Aria.classDefinition({
         runTemplateTest : function () {
             this._datePickerField = this.getInputField("datePicker");
             this.synEvent.click(this._datePickerField, {
-                fn : this._afterClicking,
+                fn : function() {this.waitforWidgetFocus("datePicker", this._afterClicking)},
                 scope : this
             });
 
